74d7ca88c318373005e146bc2d87cb59c1fd15bb
HKFPouu <389746410@qq.com>
2019-10-08 11:56:53 +0800

    feat: 反自反性、反对称性、样式

--- 离散实验/集合二元关系性质/js/index.js
+++ 离散实验/集合二元关系性质/js/index.js
@@ -31,15 +31,17 @@ const create = () => {
     }
 
     $('#list').append(`
-    <table border="1">
-        ${trList.join('')}
-    </table>
+      <div class="List">
+        <table border="1">
+          ${trList.join('')}
+        </table>
+      </div>
     `)
   }
 }
 
 const natureShow = () => {
-  let reflexivity ,symmetry, transitivity;
+  let reflexivity, symmetry, transitivity, oppsiteReflexivity, oppsiteSymmetry;
   $('#list').click((e) => {
     $(e.target).prop("nodeName") == 'TD' ? zeroExchangeOne() : ''
 
@@ -49,50 +51,103 @@ const natureShow = () => {
   })
 
   $('#natureShow').click(() => {
-    let tdList = [...$('td')], arr = [];
-
-    for (let i = 0; i < tdList.length; i + line - 1) {
-      arr.push(tdList.splice(i, i + line))
-    }
+    let trList = [...$('tr')],
+      arr = [];
+    trList.map(item => {
+      arr.push($(item).children())
+    })
 
     reflexivity = Reflexivity(arr)
     symmetry = Symmetry(arr)
     transitivity = Transitivity(arr)
+    oppsiteReflexivity = OppsiteReflexivity(arr)
+    oppsiteSymmetry = OppsiteSymmetry(arr)
+
+    $('.result').append(`
+      <p>自反性：${reflexivity}</p>
+      <p>反自反性：${oppsiteSymmetry}</p>
+      <p>对称性：${symmetry}</p>
+      <p>反对称性：${oppsiteReflexivity}</p>
+      <p>传递性：${transitivity}</p>
+    `)
   })
 
   function Reflexivity(arr) {
     let result = 'Yes';
+    for (let l = 0; l < line - 1; l++) {
+      if (arr[l][l].innerText == 0) {
+        result = 'Not'
+        break;
+      }
+    }
+    return result;
+  }
+
+  function Symmetry(arr) {
+    let result = 'Yes';
+    if (line == 1) {
+      result = 'Not';
+      return result;
+    }
     for (let l = 0; l < line; l++) {
       for (let c = 0; c < col; c++) {
-        if (arr[l][l].innerText == 0)
-          result = 'Not'
-      }
-    }
-    return result;
-  }
-
-  function Symmetry(arr){
-    let result = 'Not';
-    for (let l = 0; l < line; l++) {
-      for (let c = 0; c < col -1; c++) {
-        if ((arr[l][c].innerText == arr[c][l].innerText) && arr[l][c].innerText == 1){
-          result = 'Yes';
-          break;
-        }
-      }
-    }
-    return result;
-  }
-
-  function Transitivity(arr){
-    let result = 'Yes';
-    for (let l = 0; l < line; l++) {
-      for (let c = 0; c < col - 1; c++) {
-        for(let z = 0;z < col -1; z++){
-          if ((arr[l][c].innerText == arr[c][z].innerText) && (arr[l][c].innerText == arr[l][z].innerText) && arr[l][c].innerText == 1){
+        if (arr[c][l] && arr[l][c]) {
+          if ((arr[l][c].innerText != arr[c][l].innerText) && arr[l][c].innerText == 0) {
             result = 'Not';
             break;
+          }
         }
+      }
+    }
+    return result;
+  }
+
+  function Transitivity(arr) {
+    let result = 'Not', arr1;
+
+    arr1 = [...document.querySelectorAll("td")].filter(function (ele) {
+      return ele.innerText == 1
+    })
+    if (arr1.length == 1) {
+      result = 'Yes';
+      return result;
+    }
+
+    for (let l = 0; l < line; l++) {
+      for (let c = 0; c < col - 1; c++) {
+        for (let z = 0; z < col - 1; z++) {
+          if (arr[c][l] && arr[l][c]) {
+            if ((arr[l][c].innerText == arr[c][z].innerText) && (arr[l][c].innerText == arr[l][z].innerText) && arr[l][c].innerText == 1) {
+              result = 'Yes';
+              break;
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  function OppsiteReflexivity(arr) {
+    let result = 'Yes';
+    for (let l = 0; l < line - 1; l++) {
+      if (arr[l][l].innerText == 1) {
+        result = 'Not'
+        break;
+      }
+    }
+    return result;
+  }
+
+  function OppsiteSymmetry(arr) {
+    let result = 'Not';
+    for (let l = 0; l < line; l++) {
+      for (let c = 0; c < col - 1; c++) {
+        if (arr[c][l] && arr[l][c]) {
+          if ((arr[l][c].innerText == arr[c][l].innerText) && l == c && arr[l][c].innerText == 1) {
+            result = 'Yes';
+            break;
+          }
         }
       }
     }
